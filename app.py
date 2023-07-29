9264280516297acda729030810a8c56d28470dd7
Newton23-nk <newtonkimathi20@gmail.com>
2023-07-29 15:14:14 +0300

    App

---
+++ app.py
@@ -0,0 +1,22 @@
+# Importing Packages
+import streamlit as st
+import pandas as pd
+import os
+
+st.title("Sales Forecast App for Corporation Favorita")
+st.write("""Welcome to Corporation Favorita Sales Prediction app!
+         This app allows you to predict the Sales for a specific 
+         product in a chosen store at Corporation Favorita
+     
+     """)
+
+product_name = st.text_input("Enter the name of the product")
+Promotion = st.number_input("Enter 1 if the product was on promotion and 0 if the product was not on promotion")
+transactions = st.number_input("Enter the number of transactions for the product")
+
+
+
+
+
+
+
