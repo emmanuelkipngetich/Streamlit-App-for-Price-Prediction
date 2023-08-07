ee7baa3112cd891dab37c3a0d0589e3b21ff2c48
Newton23-nk <newtonkimathi20@gmail.com>
2023-08-07 12:26:13 +0300

    Updated

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=c:\Users\Newton\Git Hub\Linear Regression app\Sales-Prediction-App-1\.venv
+set VIRTUAL_ENV=c:\Users\Newton\Git Hub\Linear Regression app\Sales-Prediction-App-2\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
